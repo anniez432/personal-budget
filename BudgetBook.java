01e239a0e4258dc3f6a9e84ef4a27e4e6bec91e9
Annie <49576316+anniez432@users.noreply.github.com>
2024-04-25 12:19:33 -0500

    Add files via upload

---
+++ BudgetBook.java
@@ -0,0 +1,62 @@
+import java.util.Date;
+import java.util.LinkedList;
+
+/**
+ * Class that creates a budget book for the user that contains their various budgets.
+ */
+public class BudgetBook {
+
+    //data fields
+    protected String name; //name of the budget
+    protected Date date; //date of creation
+    protected int size; //number of budgets within the budget book
+
+    protected LinkedList<PersonalBudget> budgetBook;
+    private LinkedList<String> budgetNames;
+    //constructors
+    public BudgetBook(String name, Date date){
+        this.name = name;
+        this.date = date;
+        this.size = 0;
+    }
+
+    //methods
+
+    /**
+     * Adds the user's budget to the BudgetBook.
+     * @param budget The budget to be added.
+     */
+    public void addBudget(PersonalBudget budget){
+        budgetBook.add(budget);
+        addBudgetName(budget.name);
+        size++;
+    }
+
+    private void addBudgetName(String name){
+        budgetNames.add(name);
+    }
+
+    /**
+     * Removes a budget from the Budgetbook.
+     * @param budget The budget to be removed.
+     */
+    public void removeBudget(String budget){
+        if(!budgetNames.contains(budget)){
+            System.out.println("Invalid budget. Enter a valid budget name.");
+        } else {
+            int index = 0;
+           //find index of name
+            for(int i = 0; i < budgetNames.size(); ++i){
+                if(budgetNames.get(i).equals(budget)){
+                    index = i;
+                    break;
+                }
+            }
+            //remove name
+            budgetNames.remove(budget);
+            //remove budget
+            budgetBook.remove(index);
+            size--;
+        }
+    }
+}
